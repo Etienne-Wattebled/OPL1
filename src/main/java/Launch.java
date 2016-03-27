0539cbc3799bf7e150eec139c9c0c0b184b4bb36
Étienne <etienne@wattebled.fr>
2016-03-28 00:35:37 +0200

    màj

--- src/main/java/Launch.java
+++ src/main/java/Launch.java
@@ -2,7 +2,8 @@
 public class Launch {
 	public static void main(String args[]) throws Exception {
 		spoon.Launcher.main(new String[] {"-i","input","-p","com.etienne.wattebled.opl1.processors.nullpointer.SeparatesDeclarationInitialization"});
-		spoon.Launcher.main(new String[] {"-i","spooned","-p","com.etienne.wattebled.opl1.processors.nullpointer.RepairsNullPointerExceptionCtVariableRead"});
 		spoon.Launcher.main(new String[] {"-i","spooned","-p","com.etienne.wattebled.opl1.processors.nullpointer.RepairsArrayOutOfBoundExceptionCtArrayRead"});
+
+		spoon.Launcher.main(new String[] {"-i","spooned","-p","com.etienne.wattebled.opl1.processors.nullpointer.RepairsNullPointerExceptionCtVariableRead"});
 	}
 }
